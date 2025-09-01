0f3a03317f41765b341e5e9d8e7d34d7380d565e
fouzy213 <chikouchefouzy@gmail.com>
2025-09-01 16:05:45 +0200

    all_page_debug

--- controllers/AllPageController.ts
+++ controllers/AllPageController.ts
@@ -24,7 +24,7 @@ export class AllPageController{
 
   
 
-    this.response.render("./allpage", {
+    this.response.render("/pages/allpage", {
       announcements: allannounce,
     total: allannounce.length,
     });
